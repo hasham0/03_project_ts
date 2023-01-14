26ee210e75cbd80da09b60464c507525f39b7fe1
Hasham_saleem <hashamsaleem75@gmail.com>
2023-01-14 21:18:18 +0500

    done

---
+++ dist/index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// array to store todo message
+const todoList = [];
+let loop = true;
+//loop to take multiple lodo inputs from user
+while (loop) {
+    //taking input
+    const useInput = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todoMsg",
+            message: "write your message:",
+        },
+        // asking for one more message
+        {
+            type: "confirm",
+            name: "nextMsg",
+            message: "Do you want to add more todo:",
+            default: false,
+        },
+    ]);
+    const { todoMsg, nextMsg } = useInput;
+    //when nextmsg is equal to false loop ends
+    loop = nextMsg;
+    //when nextmsg is equal to true loop continue and push the value in todolist array
+    if (todoMsg) {
+        todoList.push(todoMsg);
+    }
+    else {
+        console.log("write valide message");
+    }
+}
+//In the end printing all the message in the todo list
+if (todoList.length > 0) {
+    console.log("Your todo list");
+    todoList.forEach((val) => console.log(val));
+}
+else {
+    console.log("nothing in the todolist");
+}
